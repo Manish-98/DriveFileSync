d9b03c02a9ec7bf41c7cbdeb526e06e2e6c6d913
Manish Kumar Lath <manishlath3445@gmail.com>
2022-08-17 18:46:49 +0530

    Add jacoco coverage with some exclusions

--- src/main/java/utils/Utilities.java
+++ src/main/java/utils/Utilities.java
@@ -3,6 +3,8 @@ package utils;
 import java.io.InputStream;
 
 public class Utilities {
+    private Utilities() {}
+
     public static InputStream getResourceAsStream(String resourcePath) {
         return Utilities.class.getResourceAsStream(resourcePath);
     }
